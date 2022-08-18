b0b89ee6fc18a8125f2b2a887ef4851f06a402a0
ali-dev-code <ali.asgr514@gmail.com>
2022-08-18 21:11:13 +0500

    uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+(function () {
+
+	window.onload = function () {
+		window.setTimeout(fadeout, 0);
+	};
+  function fadeout() {
+		document.querySelector(".page-loader").style.opacity = "0";
+		document.querySelector(".page-loader").style.display = "none";
+	}
+
+
+
+  //========= glightbox
+// const glightbox = GLightbox({
+//   selector: ".glightbox",
+// });
+
+})();
